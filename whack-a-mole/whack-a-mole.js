531f9ecfcca7c8e71f4f97fe0b26e619c24bf277
Graeme Ford <gkford@gmail.com>
2022-05-09 22:11:22 +1200

    Make cells clickable & create 2D array

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,46 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+console.log('Whack-a-Mole!')
+
+//- The ability to store or get references to the cells in the HTML table.
+//- A function to randomly select which cell should show the mole.
+//- A way to show a mole in the chosen cell.
+
+//get the tr items
+//get the td items inside the tr items
+//
+
+let rows = document.getElementsByTagName('TR')
+
+//for each row,
+let board = []
+let x = Array.isArray(board)
+console.log(x == undefined)
+//board.push([])
+console.log(board)
+for (let i = 0; i < rows.length; i++) {
+  board[i] = []
+  let cells = rows[i].getElementsByTagName('TD')
+  for (let j = 0; j < cells.length; j++) {
+    board[i][j] = []
+    cells[j].onclick = cellClicked
+  }
+}
+
+console.log(board)
+//for each  cell,
+//create an onclick function linking to cellclicked
+
+function cellClicked(e) {
+  let cell = e.target
+  cell.innerHTML = 'x'
+  console.log('click!')
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min) + min) //The maximum is exclusive and the minimum is inclusive
+}
