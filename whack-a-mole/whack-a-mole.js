05301ac71ca56c5a8cf7927c3c5aebaac8e71c60
Graeme Ford <gkford@gmail.com>
2022-05-09 23:21:25 +1200

    Add addMole function

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -8,39 +8,56 @@ console.log('Whack-a-Mole!')
 //- A function to randomly select which cell should show the mole.
 //- A way to show a mole in the chosen cell.
 
-//get the tr items
-//get the td items inside the tr items
-//
+function getCell(rowNum, colNum) {
+  let rows = document.getElementsByTagName('TR')
+  let cells = rows[rowNum].getElementsByTagName('TD')
+  let cell = cells[colNum]
+  return cell
+}
 
-let rows = document.getElementsByTagName('TR')
+const board = [...Array(5)].map((cell) => Array(5))
 
-//for each row,
-let board = []
-let x = Array.isArray(board)
-console.log(x == undefined)
-//board.push([])
-console.log(board)
-for (let i = 0; i < rows.length; i++) {
-  board[i] = []
-  let cells = rows[i].getElementsByTagName('TD')
-  for (let j = 0; j < cells.length; j++) {
-    board[i][j] = []
-    cells[j].onclick = cellClicked
+//console.log(board)
+
+for (let row = 0; row < board.length; row++) {
+  for (let col = 0; col < board[row].length; col++) {
+    board[row][col] = getCell(row, col)
+    board[row][col].onclick = cellClicked
   }
 }
 
+function addMole() {
+  let rowNum = getRandomInt(0, 5)
+  let colNum = getRandomInt(0, 5)
+  console.log(rowNum, colNum)
+  targetCell = board[rowNum][colNum]
+  const image = document.createElement('img')
+  image.src = 'mole.PNG'
+  image.style = 'width: 100%'
+  targetCell.appendChild(image)
+}
+
+addMole()
 console.log(board)
 //for each  cell,
 //create an onclick function linking to cellclicked
 
 function cellClicked(e) {
-  let cell = e.target
-  cell.innerHTML = 'x'
-  console.log('click!')
+  cell = e.target
+  console.log('click')
 }
 
 function getRandomInt(min, max) {
   min = Math.ceil(min)
   max = Math.floor(max)
   return Math.floor(Math.random() * (max - min) + min) //The maximum is exclusive and the minimum is inclusive
+}
+
+function getRandomCell(numRows, numCols) {
+  return [rownNum, colNum]
+}
+
+function newMole() {
+  let rowNum = getRandomInt(1, numRows + 1)
+  let colNum = getRandomInt(1, numCols + 1)
 }
